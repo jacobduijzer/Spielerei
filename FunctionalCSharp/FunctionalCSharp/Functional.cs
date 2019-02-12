a4fd2ac4d63e1edc6f8b45addb3bbcb673114551
Jacob <jacob@duijzer.com>
2019-02-12 22:00:29 +0100

    Functional tests

---
+++ FunctionalCSharp/FunctionalCSharp/Functional.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FunctionalCSharp
+{
+    public class Functional
+    {
+        private Func<int, bool> isMod2 = x => x % 2 == 0;
+        private Func<int, bool> isNotMod2 = x => x % 2 != 0;
+
+        public IEnumerable<int> GetEvenNumbers(IEnumerable<int> list) =>
+            list.Where(isMod2);
+
+        public IEnumerable<int> GetUnevenNumbers(IEnumerable<int> list) =>
+            list.Where(isNotMod2);
+    }
+
+    
+}
---
+++ FunctionalCSharp/FunctionalCSharp/FunctionalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunctionalCSharp
+{
+    public static class FunctionalExtensions
+    {
+        public static IEnumerable<T> Where<T>(
+            this IEnumerable<T> ts, 
+            Func<T, bool> predicate)
+        {
+            foreach (T t in ts)
+                if (predicate(t))
+                    yield return t;
+        }
+    }
+}
